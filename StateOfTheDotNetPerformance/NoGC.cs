c3dfd2fd344ed06d13abf50a985ffce207d63bed
Adam Sitnik <adam.sitnik@gmail.com>
2017-05-25 23:55:14 +0200

    Value Types vs Reference Types benchmarks

---
+++ StateOfTheDotNetPerformance/NoGC.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace StateOfTheDotNetPerformance
+{
+    [BenchmarkCategory("Value Types vs Reference Types")]
+    [MemoryDiagnoser]
+    public class NoGC
+    {
+        [Benchmark(Baseline = true)]
+        public (int, int) CreateValueTuple() => (0, 0);
+
+        [Benchmark]
+        public Tuple<int, int> CreateTuple() => Tuple.Create(0, 0);
+    }
+}
---
+++ StateOfTheDotNetPerformance/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace StateOfTheDotNetPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly);
+
+            BenchmarkRunner.Run<DataLocality>();
+        }
+    }
+}
